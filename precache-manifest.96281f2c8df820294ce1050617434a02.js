db1b30f73971e2de0a6a57330934eccf5a4abb76
Ihor92 <salfetnikov92@gmail.com>
2020-03-07 08:59:20 +0200

    Updates

---
+++ precache-manifest.96281f2c8df820294ce1050617434a02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1651548d7ffd88f8a49832ee95d1dced",
+    "url": "/widget-pay-kasta/index.html"
+  },
+  {
+    "revision": "2a1bf27619b876942660",
+    "url": "/widget-pay-kasta/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f81c819ced55ac307f33",
+    "url": "/widget-pay-kasta/static/css/main.07a635ed.chunk.css"
+  },
+  {
+    "revision": "2a1bf27619b876942660",
+    "url": "/widget-pay-kasta/static/js/2.4efeef20.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/widget-pay-kasta/static/js/2.4efeef20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f81c819ced55ac307f33",
+    "url": "/widget-pay-kasta/static/js/main.d2746a1b.chunk.js"
+  },
+  {
+    "revision": "76bd596f69512ec4890b",
+    "url": "/widget-pay-kasta/static/js/runtime-main.cd73a78c.js"
+  }
+]);
